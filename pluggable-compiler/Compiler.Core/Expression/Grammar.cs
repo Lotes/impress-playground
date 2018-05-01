74bc768237277791b15d8f68c5c5d3c7f7a44947
Lotes <markhoernchen@gmx.de>
2018-05-01 09:57:50 +0200

    merged generic PEG

---
+++ pluggable-compiler/Compiler.Core/Expression/Grammar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Compiler.Core.Expression
+{
+    public class Grammar : IGrammar
+    {
+        public Grammar(IEnumerable<IRule> rules, IRule startAt)
+        {
+            if (startAt == null || !rules.Contains(startAt))
+                throw new ArgumentException(nameof(startAt));
+            var realStart = new Rule(Expressions.Sequence(Expressions.Call(startAt), Expressions.EOF));
+            StartingRule = realStart;
+            Rules = rules.Concat(new[] { realStart }).ToArray();
+        }
+
+        public IRule StartingRule { get; }
+        public IEnumerable<IRule> Rules { get; }
+    }
+}
